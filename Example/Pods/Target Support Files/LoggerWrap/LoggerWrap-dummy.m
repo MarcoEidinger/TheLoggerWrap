34d2385deb804e947413b3c3d8b538e94c5f6179
Marco Eidinger <marco.eidinger@sap.com>
2021-08-05 14:59:00 -0700

    CocoaPod lib template + updates

---
+++ Example/Pods/Target Support Files/LoggerWrap/LoggerWrap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LoggerWrap : NSObject
+@end
+@implementation PodsDummy_LoggerWrap
+@end
---
+++ LoggerWrap/Classes/TheLoggerWrap.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct LoggerWrap {
+    public init() {}
+
+    public func success() {
+        print("Success")
+    }
+}
